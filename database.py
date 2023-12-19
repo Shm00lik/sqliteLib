54ba32976f98f178350266c5b1386638dd61b7dd
Shm00lik <70761433+Shm00lik@users.noreply.github.com>
2023-12-19 23:19:11 +0200

    Fixed the bug that 'execute' and 'connection' needed to be at the same thread

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+import threading
+import os
 
 class Database:
     __instance = None
@@ -17,14 +18,37 @@ class Database:
         else:
             Database.__instance = self
 
-        self.connection = sqlite3.connect(databasePath)
+        self.databasePath = databasePath
+        self.connection: sqlite3.Connection = sqlite3.connect(self.databasePath)
+        self.cursor: sqlite3.Cursor = self.connection.cursor()
+        self.queriesToExecute: list[str] = []
+
+        self.executerThread: threading.Thread = threading.Thread(target=self.run)
+
+        self.shouldRun: bool = True
+
+        self.start()
+
+    def start(self) -> None:
+        self.executerThread.start()
+
+    def run(self) -> None:
+        self.connection = sqlite3.connect(self.databasePath)
         self.cursor = self.connection.cursor()
 
-    def execute(self, *queris: str) -> None:
-        for query in queris:
-            self.cursor.execute(query)
+        while self.shouldRun:
+            if len(self.queriesToExecute) > 0:
+                self.executeQuery(self.queriesToExecute.pop(0))
 
-        self.connection.commit()
+    def execute(self, *queries: str) -> None:
+        self.queriesToExecute.append(*queries)
+
+    def executeQuery(self, query: str) -> None:
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as e:
+            print(f"Error while executing '{query}': \n" + str(e))
 
     def fetch(self, query: str) -> list[tuple]:
         self.cursor.execute(query)
@@ -39,4 +63,7 @@ class Database:
         return self.cursor.fetchmany(size)
 
     def close(self):
+        self.shouldRun = False
+        self.executerThread.join()
+        
         self.connection.close()
